f483787a950b6447dc8882b9d5d8bf628c783527
Unknown <davisk14@uni.coventry.ac.uk>
2018-04-11 18:46:36 +0100

    Editting UI design
    Setting up the UI design

--- app/src/main/java/com/example/kobej/attendancesystem/LoginActivity.java
+++ app/src/main/java/com/example/kobej/attendancesystem/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.kobej.attendancesystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+
     }
 }
